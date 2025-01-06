844c93b00c3eba5a060fa4cc8fc57bb136135591
Srikant <chaudharysrikant26@gmail.com>
2025-01-06 23:20:10 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import express, { json,urlencoded } from "express";
+import session from "express-session";
+import passport from "passport";
+import dotenv from "dotenv";
+import cors from "cors";
+import dbConnect from "./config/dbConnect.js";
+
+dotenv.config();
+
+dbConnect();
+
+
+const app = express();
+
+
+// Middleware
+
+const corsOptions = {
+    origin: ["http://localhost:3001"],
+    credentials:true,
+};
+app.use(cors(corsOptions));
+app.use(json({limit:"100mb"}));
+app.use(urlencoded({limit: "100mb",extended:true}));
+app.use(session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000*60,
+    },
+}));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+
+
+
+// Listen app
+
+const PORT = process.env.PORT || 7002;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
